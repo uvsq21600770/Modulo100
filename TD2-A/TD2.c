d401beced0582ebfefa0f1be5109dce73637e2c5
Utilisateur <user@debian.arcahe.ovh>
2021-04-20 16:53:38 +0200

    La vitesse

---
+++ TD2-A/TD2.c
@@ -0,0 +1,151 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <sys/types.h>
+#include <unistd.h>
+#include <math.h>
+/*
+double exp(double lambda)
+{
+    return -log(unif())/lambda;
+}
+
+int arriveGratuit(float p0, float p2)
+{
+    double U = unif();
+
+    if(U < p0) return 0;
+    if(U < p0 + p2) return 2;
+    else return 1;
+}*/
+
+// Simulation d'une file en TD
+// A chaque top - 1 service
+//              - 0, 1 ou 2 arrivees
+
+// Generateur aleatoire d'arrivees
+#define RM ((double)((1L<<31) - 1))
+
+double unif()
+{
+    return (double)random()/RM;
+}
+
+int generer_arrivees(double p0, double p1, double p2)
+{
+    double X = unif();
+
+    if(X < p0) return 0;
+    if(X < p0 + p1) return 1;
+    else return 2;
+}
+
+// Fonction de gestion de l'echeancier
+#define AC 1
+struct event{
+    double date;
+    int nom_event; //AC
+    struct event* suiv;
+};
+typedef struct event* ECHEANCIER;
+
+struct event *premier_evenement(ECHEANCIER *E)
+{
+    if (*E==NULL) exit(2);
+
+    struct event *premier = *E;
+    *E = (*E)->suiv;
+
+    return premier;
+}
+
+ECHEANCIER ajouter_evenement(ECHEANCIER E, double date, int nom_event)
+{
+    if ((E==NULL) || (date < E->date))
+    {
+        struct event* e = malloc(sizeof(struct event));
+        if (e==NULL) exit(3);
+
+        e->date = date;
+        e->nom_event = nom_event;
+        e->suiv = E;
+        E = e;
+
+        return E;
+    }
+
+    E = ajouter_evenement(E->suiv, date, nom_event);
+    return E;
+}
+
+#define DANSFIC
+//Fonction de simulation
+#define TMAX 1e6
+double simul (double p0, double p1, double p2)
+{
+#ifdef DANSFIC    
+    FILE *F = fopen("file.data", "w");
+    if(F == NULL) exit(4);
+#endif
+
+    // Les variables
+    unsigned long int T = 0; // Date de simulation
+    unsigned long int N = 0; // Nombre de client dans la file
+    double S = 0.0; // Somme du nombre client
+    unsigned long int DS = 10000; // La longueur du manchon
+    double h = 1e-4; // La hauteur relative du manchon
+    double m = 0.0; // Le milieu du manchon la valeur initiale est pas vraiment importante
+
+    ECHEANCIER E = NULL;
+    E = ajouter_evenement(E, 0, AC);
+    struct event *e;
+
+    while(T < TMAX && DS > 0)
+    {        
+        e = premier_evenement(&E);
+        T = e->date;
+#ifdef DANSFIC  
+fprintf(F, "%lu %lu %lf\n", T, N, S/(T+1));
+#endif
+        if (e->nom_event == AC) {
+            N = N + generer_arrivees(p0, p1, p2); // Les arrivees
+            S = S + N;
+            if(N > 0) N = N-1; // Le service
+
+            E = ajouter_evenement(E, T+1, 1);
+        }
+        double nbmoy = S/(T+1);
+        if(nbmoy > m+h/2 || nbmoy < m-h/2)
+        {
+            // Nouveau manchon
+            m = nbmoy;
+            DS = 10000;
+            h = m*1e-3;
+        }
+        else DS = DS - 1;
+
+        free(e);
+    }
+#ifdef DANSFIC      
+    fclose(F);
+#endif
+if(T < TMAX) printf("ok "); else printf("PB ");
+printf("%lu %lf\n", T, S/(T+1));
+    return S/(T+1);
+}
+
+int main()
+{
+    unsigned int seed = getpid();
+    srandom(seed);
+    double nbmoy;
+    double p0, p1, p2;
+    p0 = 0.5;
+    for(p2 = 0.2; p2 < 0.9; p2+=0.05)
+    {
+        p1 = 1.0 - p0 - p2;
+        nbmoy = simul(p0, p1, p2);
+    printf("%lf %lf %lf\n",p2 , p1+2*p2, nbmoy);
+    }
+    
+    return 0;
+}
--- TD2.c
+++ TD2.c
@@ -3,12 +3,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <math.h>
-/*
-double exp(double lambda)
-{
-    return -log(unif())/lambda;
-}
 
+/*
 int arriveGratuit(float p0, float p2)
 {
     double U = unif();
@@ -18,9 +14,10 @@ int arriveGratuit(float p0, float p2)
     else return 1;
 }*/
 
-// Simulation d'une file en TD
-// A chaque top - 1 service
-//              - 0, 1 ou 2 arrivees
+
+// Simulation d'une file en M/M/1
+
+
 
 // Generateur aleatoire d'arrivees
 #define RM ((double)((1L<<31) - 1))
@@ -28,6 +25,11 @@ int arriveGratuit(float p0, float p2)
 double unif()
 {
     return (double)random()/RM;
+}
+
+double loi_exp(double lambda)
+{
+    return -log(unif())/lambda;
 }
 
 int generer_arrivees(double p0, double p1, double p2)
@@ -41,6 +43,8 @@ int generer_arrivees(double p0, double p1, double p2)
 
 // Fonction de gestion de l'echeancier
 #define AC 1
+#define DS 2
+#define FS 3
 struct event{
     double date;
     int nom_event; //AC
@@ -73,63 +77,82 @@ ECHEANCIER ajouter_evenement(ECHEANCIER E, double date, int nom_event)
         return E;
     }
 
-    E = ajouter_evenement(E->suiv, date, nom_event);
+    E->suiv = ajouter_evenement(E->suiv, date, nom_event);
     return E;
 }
 
-#define DANSFIC
-//Fonction de simulation
-#define TMAX 1e5
-double simul (double p0, double p1, double p2)
-{
-#ifdef DANSFIC    
-    FILE *F = fopen("file.data", "w");
-    if(F == NULL) exit(4);
-#endif
-
-    // Les variables
-    unsigned long int T = 0; // Date de simulation
+// Les variables globales, ont a pas le time
+    double T = 0; // Date de simulation
     unsigned long int N = 0; // Nombre de client dans la file
-    double S = 0.0; // Somme du nombre client
+    ECHEANCIER E = NULL;     // Echenancier
 
-    ECHEANCIER E = NULL;
+    double last;
+    double S;
+
+void traiter_AC(double t, double lambda)
+{
+
+    S = S + (t-last)*N;
+    last = t;
+    N++; // Les arrivees
+    E = ajouter_evenement(E, t+loi_exp(lambda), AC);
+    if(N == 1) E = ajouter_evenement(E, t, DS);
+}
+
+void traiter_DS(double t, double mu)
+{
+
+    E = ajouter_evenement(E, t+loi_exp(mu), FS);
+}
+
+void traiter_FS(double t)
+{
+    S = S + (t-last)*N;
+    last = t;
+    N = N - 1;
+    if(N > 0) E = ajouter_evenement(E, t, DS);
+}
+
+
+//Fonction de simulation
+#define TMAX 1e6
+void simul (double lambda, double mu)
+{
+
+    // Initialisation
+    T = 0.0; // Date de simulation
+    N = 0; // Nombre de client dans la file
     E = ajouter_evenement(E, 0, AC);
     struct event *e;
 
+    last = 0.0;
+    S = 0.0;
+    unsigned long int i = 0;
     while(T < TMAX)
-    {        
+    {   
         e = premier_evenement(&E);
         T = e->date;
-#ifdef DANSFIC  
-fprintf(F, "%lu %lu %lf\n", T, N, S/(T+1));
-#endif
-        if (e->nom_event == AC) {
-            N = N + generer_arrivees(p0, p1, p2); // Les arrivees
-            S = S + N;
-            if(N > 0) N = N-1; // Le service
-
-            E = ajouter_evenement(E, T+1, 1);
-        }
+        if (e->nom_event == AC) traiter_AC(T, lambda);
+        if (e->nom_event == DS) traiter_DS(T, mu);
+        if (e->nom_event == FS) traiter_FS(T);
+        if(!i && T) printf("%lf %lf\r", T, S/T);
+        fflush(stdout);
+        free(e);
+        i = (i+1) % 10000;
     }
-#ifdef DANSFIC      
-    fclose(F);
-#endif
-    return S/(T+1);
+    printf("\n%10.2le %lf\r", T, S/T);
+
 }
 
 int main()
 {
     unsigned int seed = getpid();
-    srandom(seed);
-    double nbmoy;
-    double p0, p1, p2;
-    p0 = 0.5;
-    for(p2 = 0.0; p2 < 0.9; p2+=0.05)
-    {
-        p1 = 1.0 - p0 - p2;
-        nbmoy = simul(p0, p1, p2);
-    printf("%lf %lf %lf\n",p2 , p1+2*p2, nbmoy);
-    }
+    //srandom(seed);
+    
+    double lambda, mu;
+    lambda = 0.8;
+    mu = 1.0;
+    simul(lambda, mu);
     
     return 0;
 }
